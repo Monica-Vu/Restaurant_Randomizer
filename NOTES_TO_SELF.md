ed673ce4e9a0a477b36178bc1fe80bb7bbfc8a83
Monica <vu.monica.05@gmail.com>
2023-08-03 11:21:48 -0700

    Added feature to randomly pick a restaurant

---
+++ NOTES_TO_SELF.md
@@ -0,0 +1,9 @@
+# Frontend
+## React Fragments 
+* React Fragments allow you to return multiple elements from a React component by allowing you to group a list of children without adding an extra DOM element
+* If possible, try to use a fragment as `div` elements require more resources -> causes pages to slow down & harder to debug. Fragments have a smaller dom -> renders fast and allows multiple JSX components to be return, which addresses the issue of having invalid HTML markups
+* You could also use `<></>` 
+
+**Source(s):** https://refine.dev/blog/how-react-fragments-is-works/#what-is-react-fragment
+
+# Backend
--- frontend/src/components/RandomizeRestaurant.js
+++ frontend/src/components/RandomizeRestaurant.js
@@ -1,41 +1,56 @@
-import React, { useState, useEffect } from "react"
-import RestaurantDataService from "../services/RestaurantService"
+import React, { useState, useEffect } from "react";
+import RestaurantDataService from "../services/RestaurantService";
 
 const Randomizer = (props) => {
-    const [restaurants, setRestaurants] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+  const [randomRestaurant, setRandomRestaurant] = useState(null);
 
-    useEffect(() => {
-        retrieveRestaurants();
-    }, []);
+  useEffect(() => {
+    retrieveRestaurants();
+  }, []);
 
-    const retrieveRestaurants = () => {
-        RestaurantDataService.getAll()
-            .then(response => {
-                setRestaurants(response.data);
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    };
-
-    const retrieveList = () => {
-        retrieveRestaurants();
+  const retrieveRestaurants = async () => {
+    try {
+      const response = await RestaurantDataService.getAll();
+      setRestaurants(response.data);
+      console.log("retrieveRestaurants =>", response.data);
+    } catch (error) {
+      console.log("Error while retrieving restaurants:", error);
     }
+  };
+
+  const getRandomRestaurant = () => {
+    if (restaurants.length === 0) {
+      // If the list is empty, do nothing
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * restaurants.length);
+    const randomRestaurant = restaurants[randomIndex];
+    setRandomRestaurant(randomRestaurant);
+  };
 
   return (
     <div>
-        <h1> </h1>
-        {restaurants &&
-          restaurants.map((restaurant) => (
-              <p> 
-              {restaurant.name}          
-              </p>
+      <h3>
+        <b>Restaurant List</b>
+      </h3>
+      {restaurants && (
+        <p>
+          {restaurants.map((restaurant, index) => (
+            <React.Fragment key={index}>
+              {restaurant.name}
+              {index !== restaurants.length - 1 ? ", " : ""}
+            </React.Fragment>
           ))}
+        </p>
+      )}
 
-      <button className="btn btn-md btn-primary" onClick={retrieveList}>
+      <button className="btn btn-md btn-primary mb-2" onClick={getRandomRestaurant}>
         Randomize
       </button>
+      
+      {randomRestaurant && (<p> {randomRestaurant.name} </p>)}
     </div>
   );
 };
